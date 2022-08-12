c6c3cbe1f989570486c64a5d9ebd814fa6f085a2
Marwa <marwahakmi@gmail.com>
2022-08-12 12:11:05 +0300

    assignement Completed

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // code your solution here
+const saturdayFun = param => {
+    if (param) return `This Saturday, I want to ${param}!`
+    else return "This Saturday, I want to roller-skate!"
+}
+
+const mondayWork = param => {
+    if (param) return `This Monday, I will ${param}.`
+    else return 'This Monday, I will go to the office.'
+}
+
+const wrapAdjective = param => str => `You are ${param}${str}${param}!`
+
+// Executions
+console.log(saturdayFun())
+console.log(saturdayFun("bathe my dog"))
+console.log(mondayWork())
+console.log(mondayWork("work from home"))
+console.log(wrapAdjective("%")("a dedicated programmer"))
